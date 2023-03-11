412e78f3ef342a0dba57639f8ed7d47a41f34fdb
XiaMoHuaHuo_CN <2549619802@qq.com>
2023-03-11 17:33:05 +0800

    Update CommandExample.java

--- src/main/java/com/example/demo/CommandExample.java
+++ src/main/java/com/example/demo/CommandExample.java
@@ -35,7 +35,7 @@ public class CommandExample extends JavaPlugin implements Listener {
     }
 
     /**
-     * 私聊消息命令示例
+     * 群聊消息命令示例
      * MiraiMC不提供任何命令解析器，请自行处理
      * @param e 被动收到群聊消息事件
      */
